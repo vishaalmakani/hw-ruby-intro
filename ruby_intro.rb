cf2d88aff4eded72902c8922c5b3a027d099d7df
vishaalmakani <55243660+vishaalmakani@users.noreply.github.com>
2019-09-16 23:17:26 -0500

    Part 1: Implemented and tested sum(arr)
    Implemented sum(arr) method as described in part 1, and tested using sum method's tests described in 'spec/part1_spec.rb'

---
+++ ruby_intro.rb
@@ -0,0 +1,41 @@
+# When done, submit this entire file to the autograder.
+
+# Part 1
+
+def sum arr
+  sum = 0
+  if arr.empty? == true
+    return 0
+  else
+    arr.each { |x| sum+=x}
+    return sum
+  end
+end
+
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
+
+# Part 2
+
+def hello(name)
+  # YOUR CODE HERE
+end
+
+def starts_with_consonant? s
+  # YOUR CODE HERE
+end
+
+def binary_multiple_of_4? s
+  # YOUR CODE HERE
+end
+
+# Part 3
+
+class BookInStock
+# YOUR CODE HERE
+end
